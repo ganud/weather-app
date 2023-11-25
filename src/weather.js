209a7aa7cb08f82834ad2f99aaefdb4f00b292be
Dundo <107737568+ganud@users.noreply.github.com>
2023-11-25 08:31:58 -0500

    Fix typo

--- src/weather.js
+++ src/weather.js
@@ -3,15 +3,14 @@ export function writeDOM(weatherObject) {
   document.getElementById('condition').innerHTML = weatherObject.condition;
   document.getElementById('temperature').innerHTML = `${weatherObject.temperature_c}°C | ${weatherObject.temperature_f}°F`;
   document.getElementById('humidity').innerHTML = `Humidity: ${weatherObject.humidity} %`;
-  document.getElementById('wind-speed').innerHTML = `Wind Speed: ${weatherObject.windspeed} km/hr %`;
+  document.getElementById('wind-speed').innerHTML = `Wind Speed: ${weatherObject.windspeed} km/hr`;
 }
 
 export async function getData(key, location) {
   try {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`, { mode: 'cors' });
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`, { mode: 'cors' } );
     const weatherData = await response.json();
     const weatherObject = {
-      // current, location
       name: weatherData.location.name,
       country: weatherData.location.country,
       condition: weatherData.current.condition.text,
@@ -23,14 +22,15 @@ export async function getData(key, location) {
     // I can't index properties of weatherObject outside the async function, so this will have to do
     writeDOM(weatherObject);
     return weatherObject;
-  } catch (error) {
+  }
+  catch (error){
     alert('Enter a valid location');
   }
 }
 
 export function search(KEY) {
   const search = document.getElementById('search');
-  getData(KEY, search.value);
+  getData(KEY, search.value)
   search.value = '';
 }
 
